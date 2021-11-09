2307b0f2ffa81d11f0171f93329767f634382f02
Zhenchi <zhongzc_arch@outlook.com>
2021-11-09 16:01:54 +0800

    diable top sql by default & respect pd variable (#12)
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- config/pdvariable/pdvariable.go
+++ config/pdvariable/pdvariable.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/pingcap/log"
@@ -21,34 +22,43 @@ const (
 	defaultRetryInterval = time.Millisecond * 200
 )
 
-var loader *PDVariableLoader
-
-type PDVariableLoader struct {
-	getCli func() *clientv3.Client
-	cfg    *PDVariable
-	cancel context.CancelFunc
-}
-
-type PDVariable struct {
-	EnableTopSQL bool
-}
-
 func Init(getCli func() *clientv3.Client) {
-	loader = &PDVariableLoader{
+	loader = &variableLoader{
 		getCli: getCli,
 	}
 	go utils.GoWithRecovery(loader.start, nil)
 	return
 }
 
-func GetPDVariable() *PDVariable {
-	return loader.cfg
+type PDVariable struct {
+	EnableTopSQL bool
 }
 
-func (g *PDVariableLoader) start() {
+func DefaultPDVariable() *PDVariable {
+	return &PDVariable{EnableTopSQL: false}
+}
+
+type Subscriber = chan *PDVariable
+
+func Subscribe() Subscriber {
+	return loader.subscribe()
+}
+
+var loader *variableLoader
+
+type variableLoader struct {
+	getCli func() *clientv3.Client
+	cfg    *PDVariable
+	cancel context.CancelFunc
+
+	sync.Mutex
+	subscribers []Subscriber
+}
+
+func (l *variableLoader) start() {
 	ctx, cancel := context.WithCancel(context.Background())
-	g.cancel = cancel
-	g.loadGlobalConfigLoop(ctx)
+	l.cancel = cancel
+	l.loadGlobalConfigLoop(ctx)
 }
 
 func Stop() {
@@ -57,56 +67,61 @@ func Stop() {
 	}
 }
 
-func (g *PDVariableLoader) loadGlobalConfigLoop(ctx context.Context) {
+func (l *variableLoader) loadGlobalConfigLoop(ctx context.Context) {
 	ticker := time.NewTicker(time.Minute)
-	watchCh := g.getCli().Watch(ctx, globalConfigPath, clientv3.WithPrefix())
+	watchCh := l.getCli().Watch(ctx, globalConfigPath, clientv3.WithPrefix())
+
 	var err error
-	g.cfg, err = g.loadAllGlobalConfig(ctx)
+	l.cfg, err = l.loadAllGlobalConfig(ctx)
 	if err != nil {
 		log.Error("first load global config failed", zap.Error(err))
 	} else {
-		log.Info("first load global config", zap.Reflect("global-config", g.cfg))
+		log.Info("first load global config", zap.Reflect("global-config", l.cfg))
+		l.notifySubscriber()
 	}
+
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			cfg, err := g.loadAllGlobalConfig(ctx)
+			cfg, err := l.loadAllGlobalConfig(ctx)
 			if err != nil {
 				log.Error("load global config failed", zap.Error(err))
 			} else if cfg != nil {
-				if g.cfg == nil && *g.cfg != *cfg {
-					g.cfg = cfg
-					log.Info("load global config", zap.Reflect("cfg", g.cfg))
+				if l.cfg == nil || *l.cfg != *cfg {
+					l.cfg = cfg
+					log.Info("load global config", zap.Reflect("cfg", l.cfg))
+					l.notifySubscriber()
 				}
 			}
 		case e, ok := <-watchCh:
 			if !ok {
 				log.Info("global config watch channel closed")
-				watchCh = g.getCli().Watch(ctx, globalConfigPath, clientv3.WithPrefix())
+				watchCh = l.getCli().Watch(ctx, globalConfigPath, clientv3.WithPrefix())
 				// sleep a while to avoid too often.
 				time.Sleep(time.Second)
 			} else {
-				if g.cfg == nil {
-					g.cfg = &PDVariable{}
+				if l.cfg == nil {
+					l.cfg = DefaultPDVariable()
 				}
 				for _, event := range e.Events {
 					if event.Type != mvccpb.PUT {
 						continue
 					}
-					err = g.parseGlobalConfig(string(event.Kv.Key), string(event.Kv.Value), g.cfg)
+					err = l.parseGlobalConfig(string(event.Kv.Key), string(event.Kv.Value), l.cfg)
 					if err != nil {
 						log.Error("load global config failed", zap.Error(err))
 					}
-					log.Info("watch global config changed", zap.Reflect("cfg", g.cfg))
+					log.Info("watch global config changed", zap.Reflect("cfg", l.cfg))
 				}
+				l.notifySubscriber()
 			}
 		}
 	}
 }
 
-func (g *PDVariableLoader) loadAllGlobalConfig(ctx context.Context) (*PDVariable, error) {
+func (l *variableLoader) loadAllGlobalConfig(ctx context.Context) (*PDVariable, error) {
 	var err error
 	var resp *clientv3.GetResponse
 	for i := 0; i < defaultRetryCnt; i++ {
@@ -116,29 +131,29 @@ func (g *PDVariableLoader) loadAllGlobalConfig(ctx context.Context) (*PDVariable
 		default:
 		}
 		childCtx, cancel := context.WithTimeout(ctx, defaultTimeout)
-		resp, err = g.getCli().Get(childCtx, globalConfigPath, clientv3.WithPrefix())
+		resp, err = l.getCli().Get(childCtx, globalConfigPath, clientv3.WithPrefix())
 		cancel()
 		if err != nil {
 			log.Debug("load global config failed.", zap.Error(err))
 			time.Sleep(defaultRetryInterval)
 			continue
 		}
+		cfg := DefaultPDVariable()
 		if len(resp.Kvs) == 0 {
-			return nil, nil
+			return cfg, nil
 		}
-		cfg := PDVariable{}
 		for _, kv := range resp.Kvs {
-			err = g.parseGlobalConfig(string(kv.Key), string(kv.Value), &cfg)
+			err = l.parseGlobalConfig(string(kv.Key), string(kv.Value), cfg)
 			if err != nil {
 				return nil, err
 			}
 		}
-		return &cfg, nil
+		return cfg, nil
 	}
 	return nil, err
 }
 
-func (g *PDVariableLoader) parseGlobalConfig(key, value string, cfg *PDVariable) error {
+func (l *variableLoader) parseGlobalConfig(key, value string, cfg *PDVariable) error {
 	if strings.HasPrefix(key, globalConfigPath) {
 		key = key[len(globalConfigPath):]
 	}
@@ -152,4 +167,25 @@ func (g *PDVariableLoader) parseGlobalConfig(key, value string, cfg *PDVariable)
 		cfg.EnableTopSQL = v
 	}
 	return nil
+}
+
+func (l *variableLoader) subscribe() Subscriber {
+	ch := make(Subscriber, 1)
+	l.Lock()
+	l.subscribers = append(l.subscribers, ch)
+	l.Unlock()
+	return ch
+}
+
+func (l *variableLoader) notifySubscriber() {
+	l.Lock()
+
+	for _, ch := range l.subscribers {
+		select {
+		case ch <- l.cfg:
+		default:
+		}
+	}
+
+	l.Unlock()
 }
