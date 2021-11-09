f713d7b515997a483bea0be43426bdb668454b64
crazycs520 <crazycs520@gmail.com>
2021-11-09 17:17:58 +0800

    refine log
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- database/document/document.go
+++ database/document/document.go
@@ -71,22 +71,20 @@ func runValueLogGC(db *badger.DB) {
 				zap.Stack("stack trace"))
 		}
 	}()
-	err := doValueLogGC(db)
-	if err != nil && err != badger.ErrNoRewrite {
-		log.Error("badger run value log gc failed", zap.Error(err))
-	}
-}
 
-func doValueLogGC(db *badger.DB) error {
 	// at most do 10 value log gc each time.
 	for i := 0; i < 10; i++ {
 		err := db.RunValueLogGC(0.1)
 		if err != nil {
-			return err
+			if err == badger.ErrNoRewrite {
+				log.Info("badger has no value log need gc now")
+			} else {
+				log.Error("badger run value log gc failed", zap.Error(err))
+			}
+			return
 		}
 		log.Info("badger run value log gc success")
 	}
-	return nil
 }
 
 func Get() *genji.DB {
