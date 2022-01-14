be2ee733e9ad7a4d9e735495918f6f3f91fd98e8
crazycs <crazycs520@gmail.com>
2022-01-14 17:55:43 +0800

    config: refine advertise address config (#101)
    close pingcap/ng-monitoring#57

--- config/config.go
+++ config/config.go
@@ -5,8 +5,10 @@ import (
 	"crypto/tls"
 	"fmt"
 	stdlog "log"
+	"net"
 	"path"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -15,6 +17,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/procutil"
 	"github.com/pingcap/log"
+	"github.com/pingcap/ng-monitoring/utils"
 	commonconfig "github.com/prometheus/common/config"
 	"go.etcd.io/etcd/pkg/transport"
 	"go.uber.org/zap"
@@ -41,7 +44,7 @@ type Config struct {
 var defaultConfig = Config{
 	Address: "0.0.0.0:12020",
 	PD: PD{
-		Endpoints: nil,
+		Endpoints: []string{"127.0.0.1:2379"},
 	},
 	Log: Log{
 		Path:  "", // default output is stdout
@@ -118,9 +121,8 @@ func InitConfig(configPath string, override func(config *Config)) (*Config, erro
 
 	override(&config)
 
-	if config.AdvertiseAddress == "" {
-		config.AdvertiseAddress = config.Address
-	}
+	config.trimFiledSpace()
+	config.setDefaultAdvertiseAddress()
 
 	if err := config.valid(); err != nil {
 		return nil, err
@@ -129,13 +131,39 @@ func InitConfig(configPath string, override func(config *Config)) (*Config, erro
 	return &config, nil
 }
 
+func (c *Config) trimFiledSpace() {
+	c.Address = strings.TrimSpace(c.Address)
+	c.AdvertiseAddress = strings.TrimSpace(c.AdvertiseAddress)
+	for i, addr := range c.PD.Endpoints {
+		c.PD.Endpoints[i] = strings.TrimSpace(addr)
+	}
+}
+
 func (c *Config) Load(fileName string) error {
 	_, err := toml.DecodeFile(fileName, c)
 	return err
 }
 
+func (c *Config) setDefaultAdvertiseAddress() {
+	if len(c.AdvertiseAddress) == 0 && strings.HasPrefix(c.Address, "0.0.0.0") {
+		ip := utils.GetLocalIP()
+		c.AdvertiseAddress = strings.Replace(c.Address, "0.0.0.0", ip, 1)
+	}
+	if len(c.AdvertiseAddress) == 0 {
+		c.AdvertiseAddress = c.Address
+	}
+}
+
 func (c *Config) valid() error {
 	var err error
+
+	if err = validateAddress(c.Address, "address"); err != nil {
+		return err
+	}
+
+	if err = validateAddress(c.AdvertiseAddress, "advertise-address"); err != nil {
+		return err
+	}
 
 	if len(c.Address) == 0 {
 		return fmt.Errorf("unexpected empty address")
@@ -153,6 +181,24 @@ func (c *Config) valid() error {
 		return err
 	}
 
+	return nil
+}
+
+func validateAddress(address, name string) error {
+	if len(address) == 0 {
+		return fmt.Errorf("unexpected empty %v", name)
+	}
+	_, port, err := net.SplitHostPort(address)
+	if err == nil {
+		var p int
+		p, err = strconv.Atoi(port)
+		if err == nil && p == 0 {
+			err = fmt.Errorf("port cannot be set to 0")
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("%v %v is invalid, err: %v", name, address, err)
+	}
 	return nil
 }
 
