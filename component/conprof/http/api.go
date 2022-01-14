c0417aa97f0c738e6f44e7eea094620046705a95
crazycs520 <crazycs520@gmail.com>
2022-01-14 11:37:52 +0800

    fix
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- component/conprof/http/api.go
+++ component/conprof/http/api.go
@@ -356,7 +356,7 @@ func queryAndDownload(c *gin.Context) error {
 }
 
 const downloadReadme = `
-To review the profile data which file name suffix is `.proto` interactively:
+To review the profile data whose file name suffix is '.proto' interactively:
 
 $ go tool pprof --http=127.0.0.1:6060 profile_xxx.proto
 `
