e538f02840763dd2a576e18bb8044cda4828ea49
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 20:18:48 +0800

    Delete component directory

--- component/topsql/query/query.go
+++
@@ -1,8 +0,0 @@
-package query
-
-type Query interface {
-	Records(name string, startSecs, endSecs, windowSecs, top int, instance, instanceType string, fill *[]RecordItem) error
-	Summary(startSecs, endSecs, windowSecs, top int, instance, instanceType string, fill *[]SummaryItem) error
-	Instances(startSecs, endSecs int, fill *[]InstanceItem) error
-	Close()
-}
